66adc897b79ffdf8d366b6da1ddffdb7e3c4b5ba
cjhalak616-bit <cjhalak616@gmail.com>
2026-06-30 09:48:58 +0530

    javascript

---
+++ javascript/condition.js
@@ -0,0 +1,37 @@
+//num=40;
+
+//if(num%2===0){
+    //console.log('even');
+    //let msg='hello';
+    //const pi=3.14;
+    
+    //console.log(msg);
+
+//}
+//else{
+  //  console.log('odd');
+//}
+
+//check if a person is eligible for DL
+
+//const age=20;
+//if(age>=18){
+  //  console.log('eligible for driving licence');
+//}
+//else{
+  //  console.log('not eligible for driving licence');
+//}
+
+//check if the triangle is equivalateral,isoceles or scalene
+const side1=30;
+const side2=40;
+const side3=30;
+if(side1===side2 && side2===side3){
+    console.log('equilateral triangle');
+}
+else if(side1===side2||side2===side3||side1===side3){
+    console.log('Isosceles triangle');
+}
+else{
+    console.log('Scalene triangle');
+}
---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+console.log('hello world');
+
+num=45;
+console.log(num);
+console.log(typeof(num));
+
+myname="jhalak";
+console.log(typeof(myname));
+
+isHungry=false;
+console.log(typeof(ishungry));
